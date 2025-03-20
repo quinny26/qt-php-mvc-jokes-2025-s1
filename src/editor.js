39828709b830a1b4cbb7f8d157ca6804bf891101
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2025-03-20 17:21:56 +0800

    feat(Products): Customise Styles
    Custom CSS for editor and descriptions
    - H1-H3
    - P
    - UL,OL
    - A

--- src/editor.js
+++ src/editor.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         ClassicEditor
             .create(messageEditor, {
                 licenseKey: 'GPL', // Use license key for commercial applications, or 'GPL' for Open source Projects.
-                toolbar: ['heading', '|', 'bold', 'italic', '|', 'link', '|', 'redo', 'undo' ],
+                toolbar: ['heading', '|', 'bold', 'italic', '|', 'numberedList', 'bulletedList', '|', 'redo', 'undo' ],
                 placeholder: 'Enter your message here...'
             })
             .catch(error => {
